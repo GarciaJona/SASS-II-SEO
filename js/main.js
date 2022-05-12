633bdb9b496a36687e160fe8b369dc42d259b934
joalega <jalexisgarcia.91@gmail.com>
2022-05-12 13:32:49 -0300

    Entrega de sass II y SEO

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-<script>
-$(document).ready(function(){
-    $('.zoom').hover(function() {
+$(document).ready(function () {
+    $('.zoom').hover(function () {
         $(this).addClass('transition');
-    }, function() {
+    }, function () {
         $(this).removeClass('transition');
     });
 });
-</script>
+
+const hamburger = document.querySelector(".hamburger");
+console.log(hamburger)
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    console.log("entro aca")
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
